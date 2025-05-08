247659df8edd30deb8eeedb71dca14d46f160dc8
chernj1 <chernj1@D202-78647>
2025-05-08 16:33:26 +1200

    Added max-width style

--- src/lib/Picture.svelte
+++ src/lib/Picture.svelte
@@ -6,3 +6,9 @@
     <img src="https://jjransom.wordpress.com/wp-content/uploads/2016/02/samurai1.gif" alt="samurai">
     <figcaption>@ngaaats</figcaption>
 </figure>
+
+<style>
+    img {
+        max-width: 150px;
+    }
+</style>
