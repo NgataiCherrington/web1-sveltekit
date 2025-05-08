a571a93bb9f50c880809ffcdf899749f51c4aee8
chernj1 <chernj1@D202-78647>
2025-05-08 16:18:13 +1200

    Create main page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Footer from "$lib/Footer.svelte";
+    import Card from "$lib/Card.svelte";
+</script>
+
+<Header />
+
+<main>
+    <Card />
+    <Card />
+</main>
+
+<Footer />
+
+<style>
+    main {
+        display: flex;
+    }
+</style>
