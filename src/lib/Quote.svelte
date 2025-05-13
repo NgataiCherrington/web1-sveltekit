a5bccd7d9cdf00bc7b024c4b185b9b26545f1acc
chernj1 <chernj1@D202-78647>
2025-05-13 15:23:28 +1200

    Using javascipt for quote

--- src/lib/Quote.svelte
+++ src/lib/Quote.svelte
@@ -1,10 +1,12 @@
 <script>
-
+    let quote = {
+        quoteText: "I think therefore, I am"
+    }
 </script>
 
 <div>
     <blockquote>
-        <p>I think therefore, I am</p>
+        <p>{quote.quoteText}</p>
     </blockquote>
     <p>-Rene Descartes</p>
 </div>
