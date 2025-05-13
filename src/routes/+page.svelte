b9071edab45a39f99261fb4a7804b91e5fd7c500
chernj1 <chernj1@D202-78647>
2025-05-13 15:46:20 +1200

    Added props data to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,15 @@
     import Footer from "$lib/Footer.svelte";
     import Card from "$lib/Card.svelte";
     import Quote from "$lib/Quote.svelte";
+
+    let quote = {
+        quoteText: "I think therefore, I am",
+        author: "Rene Descartes"
+    }
 </script>
 
 <Header />
-<Quote />
+<Quote {quote}/>
 
 <main class="card-grid">
     <Card />
