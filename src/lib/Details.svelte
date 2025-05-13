ff024489299cd9be6067a91944c13d025e913418
chernj1 <chernj1@D202-78647>
2025-05-13 15:38:02 +1200

    Added font-family

--- src/lib/Details.svelte
+++ src/lib/Details.svelte
@@ -18,5 +18,7 @@
 </section>
 
 <style>
-
+    section {
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
 </style>
