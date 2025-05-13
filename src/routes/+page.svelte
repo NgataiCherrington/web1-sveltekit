8df9154d4a4d64a25333743a3fd9b2ef1d39d761
chernj1 <chernj1@D202-78647>
2025-05-13 15:21:15 +1200

    Imported Quote component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,11 @@
     import Header from "$lib/Header.svelte"
     import Footer from "$lib/Footer.svelte";
     import Card from "$lib/Card.svelte";
+    import Quote from "$lib/Quote.svelte";
 </script>
 
 <Header />
+<Quote />
 
 <main class="card-grid">
     <Card />
