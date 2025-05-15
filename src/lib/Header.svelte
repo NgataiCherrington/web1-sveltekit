a64fee931fbf5d11623211972046293fb624e7db
chernj1 <chernj1@D202-78647>
2025-05-15 15:42:10 +1200

    Styled header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -4,3 +4,11 @@
 
 <h1>Ngatai's Website </h1>
 <Navigation />
+
+<style>
+    h1{
+        text-align: center;
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        
+    }
+</style>
