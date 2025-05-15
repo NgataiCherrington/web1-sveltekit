c902134eb94243f640ddc1c852d8f5148d606082
chernj1 <chernj1@D202-78647>
2025-05-15 16:04:49 +1200

    Created more blog pages

---
+++ src/routes/blog/[blogs]/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Header from "$lib/Header.svelte";
+    import Blog from "$lib/Blog.svelte";
+</script>
+
+<Header />
+
+<h2>My Blog</h2>
+
+<Blog />
+
+<ul>
+    <li><a href="/blog/one">Blog one</li>
+    <li><a href="/blog/two">Blog two</li>
+    <li><a href="/blog/three">Blog three</li>
+    <li><a href="/blog/four">Blog four</li>
+</ul>
