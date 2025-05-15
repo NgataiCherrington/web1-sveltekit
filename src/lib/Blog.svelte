c00127686869af3f6e306c9d3854edb5d4ec1646
chernj1 <chernj1@D202-78647>
2025-05-15 16:04:15 +1200

    Create blog component

---
+++ src/lib/Blog.svelte
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+<h2>Ngatai's Blog</h2>
+
+<form action="" method="get" class="form">
+    <div class="form">
+        <label for="name">Enter your name:</label>
+        <input type="text" name="name" id="name" required/>
+    </div>
+    <div class="form">
+        <label for="email">Enter your email:</label>
+        <input type="email" name="email" id="email" required/>
+    </div>
+</form>
+
+<style>
+    .form {
+        margin-top: 2%;
+    }
+</style>
