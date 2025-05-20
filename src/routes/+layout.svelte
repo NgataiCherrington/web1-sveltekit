c65aafa30feeaa67cf979b38958558abef35565f
chernj1 <chernj1@D202-78647>
2025-05-20 16:15:31 +1200

    Add footer to main layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,18 @@
 <script>
+    import Footer from "$lib/Footer.svelte";
+    import Header from "$lib/Header.svelte";
+    
     let  { children } = $props();
 </script>
 
-
+<!-- Include the main heading and navigation -->
+<Header />
 
 <!-- This is where the page content will go-->
 {@render children()}
+
+<!-- Include footer -->
+<Footer />
 
 <style>
 
